07c36bb4960aec7118890468f66a756d8a0d30f1
krish-patel-simform <krish.patel@simformsolutions.com>
2026-08-06 16:10:13 +0530

    feat:Implement the signaling using web socket

--- src/config/socket.ts
+++ src/config/socket.ts
@@ -9,18 +9,36 @@ export function initSocket(server: Server) {
   });
 
   const emailToSocketMap = new Map();
+  const socketIdToEmailMap = new Map();
 
   io.on('connection', (socket) => {
     console.log('new Socket connection is created');
 
-    socket.on('join-room', ({ roomId, email }) => {
+    socket.on('join-room', ({ roomId, email }, callback) => {
       socket.join(roomId);
-      console.log('A new User is joined in the room ', roomId);
+      console.log(`A new User ${email} is joined in the room ${roomId}`);
       // need to store the email in the map
       emailToSocketMap.set(email, socket.id);
+      socketIdToEmailMap.set(socket.id, email);
 
       // broadcats this to rest all in the room
-      socket.broadcast.to(roomId).emit('new-user', email);
+      socket.broadcast.to(roomId).emit('user-joined', email);
+      callback({
+        success: true,
+      });
+    });
+
+    socket.on('call-user', ({ newUserEmail, offer }) => {
+      const newUserSocketId = emailToSocketMap.get(newUserEmail);
+      const fromUserEmail = socketIdToEmailMap.get(socket.id);
+
+      socket.to(newUserSocketId).emit('incomming-call', { offer, fromUserEmail });
+    });
+
+    socket.on('call-accepted', ({ emailId, answer }) => {
+      const socketId = emailToSocketMap.get(emailId);
+
+      socket.to(socketId).emit('call-accepted', answer);
     });
 
     socket.on('disconnect', (reason) => {
